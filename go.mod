68b4f060c6b9542f34d99346259e28dd088b1c24
tony.z <tony.z@logtec.com>
2024-10-31 11:09:16 +0800

    feat: add gin

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gin-action
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
+
+func main() {
+    r := gin.Default()
+
+    // 根路由，返回简单的 Hello World 文本
+    r.GET("/", func(c *gin.Context) {
+        c.String(http.StatusOK, "Hello World!")
+    })
+
+    // /ping 路由，返回一个简单的 JSON 响应
+    r.GET("/ping", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "message": "pong",
+        })
+    })
+
+    // 运行服务器，默认监听 8080 端口
+    r.Run() // 监听并在 0.0.0.0:8080 上启动服务
+}
+
